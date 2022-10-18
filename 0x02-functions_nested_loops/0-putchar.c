6ec61b6e62958ca17e17fb2abfe098d1f7274a79
fasheazi <fasogbaezekiel2018@gmali.com>
2022-10-18 13:46:38 +0100

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - start
+ *
+ * Return: AS Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	char c;
+	char j[] = "fash\n";
+
+	while (i <= 9)
+	{
+		c = j[i];
+		_putchar(c);
+		i++;
+	}
+		-putchar('\n');
+	return (0);
+}
