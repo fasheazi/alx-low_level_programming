a09f4d54bd8d200edf962a6fc30f376fb75b6b69
fasheazi <fasogbaezekiel2018@gmali.com>
2022-10-18 23:37:20 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,22 @@
+/*Fash did this*/
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main -  multiples of 3 or 5.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
