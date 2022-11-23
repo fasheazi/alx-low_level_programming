cf94b91479dffef2d6de9d75816bbbdf5a090d2f
Ezekiel <fasogbaezequiel@gmail.com>
2022-11-23 05:20:10 +0100

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,106 +1,67 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * Error - prints Error str.
- *
- * Return: Error and new line.
+ * _isdigit - tells if the string consists of digits
+ * @argv: pointer to current item in argument
+ * Return: return 0 if all digits, 1 if not all digits.
  */
-void Error(void)
+int _isdigit(char *argv)
 {
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-	exit(98);
-}
-/**
- * allDigits - checks if all the characters are digits.
- * @arg: the arguments of program.
- * Return: 1 all digits 0 otherwise.
- */
-int allDigits(char **arg)
-{
-	int i, j;
+	int i;
 
-	for (i = 1; i <= 2; i++)
-		for (j = 0; arg[i][j]; j++)
-		{
-			if (arg[i][j] < '0' || arg[i][j] > '9')
-				return (0);
-		}
-	return (1);
-}
-/**
- * _calloc- initializes memory spaces with zero.
- * @nmemb: string 1.
- * @size: string 2, concatenated to 1
- *
- * Return: pointer to the concatenated string.
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i;
-	char *newArray;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	newArray = malloc(nmemb * size);
-	if (newArray == NULL)
-		return (NULL);
-
-	for (i = 0; i < (nmemb * size); i++)
-		*(newArray + i) = 0;
-
-	return (newArray);
-}
-/**
- * main- multiplies 2 positive numbers.
- * @argc: counter of arguments.
- * @argv: vector of arguments
- * Return: ans or Error.
- */
-
-int main(int argc, char **argv)
-{
-	int i, j, carry, len, len_s1 = 0, len_s2 = 0;
-	char *s1 = *(argv + 1), *s2 = *(argv + 2);
-	int *a, *b, *ans;
-
-	if (argc != 3 || allDigits(argv) != 1)
-		Error();
-	if (*s1 == '0' || *s2 == '0')
-		_putchar('0');
-	while (*(*(argv + 1) + len_s1))
-		len_s1++;
-	while (*(*(argv + 2) + len_s2))
-		len_s2++;
-	len = len_s1 + len_s2 + 1;
-	a = (int *) malloc(len_s1 * sizeof(int));
-	b = (int *) malloc(len_s2 * sizeof(int));
-	ans = _calloc(len, sizeof(int));
-	if (a == NULL || b == NULL || ans == NULL)
-		Error();
-	for (i = len_s1 - 1, j = 0; i >= 0; i--, j++)
-		*(a + j) = *(s1 + i) - '0';
-	for (i = len_s2 - 1, j = 0; i >= 0; i--, j++)
-		*(b + j) = *(s2 + i) - '0';
-	for (i = 0; i < len_s2; i++)
-		for (j = 0; j < len_s1; j++)
-			*(ans + i + j) = *(ans + i + j) + *(b + i) * *(a + j);
-	for (i = 0; i < len_s1 + len_s2; i++)
+	i = 0;
+	while (argv[i])
 	{
-		carry = *(ans + i) / 10, *(ans + i) = *(ans + i) % 10;
-		*(ans + i + 1) = *(ans + i + 1) + carry; }
-	for (i = len_s1 + len_s2; i >= 0; i--)
-		if (*(ans + i) > 0)
-			break;
-	for (; i >= 0; i--)
-		_putchar(*(ans + i) + '0');
-	_putchar('\n');
-	free(a), free(b), free(ans);
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+/**
+ * _atoi - converts a string of ascii digits to the values they represent
+ * @s: pointer to the source string
+ * Return: value of digits
+ */
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+/**
+ * main - main function call
+ * @argc: argument count
+ * @argv: 2D array of arguments
+ * Return: return 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 	return (0);
 }
